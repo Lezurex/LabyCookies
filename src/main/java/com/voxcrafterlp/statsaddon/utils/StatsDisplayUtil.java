3d920b2042d3702a6c9bc5c7e507664dff67ed61
BuildTools <lenny.angst@easyid.ch>
2020-12-31 14:57:20 +0100

    Fix stats command being spammed in Lobby

--- src/main/java/com/voxcrafterlp/statsaddon/utils/StatsDisplayUtil.java
+++ src/main/java/com/voxcrafterlp/statsaddon/utils/StatsDisplayUtil.java
@@ -17,20 +17,23 @@ public class StatsDisplayUtil {
     public void displayStats(List<String> playerNames) {
         new Thread(() -> {
             for(String string: playerNames) {
-                if(!string.equals(Minecraft.getMinecraft().thePlayer.getGameProfile().getName())) {
-                    if (!StatsAddon.getInstance().checkedPlayers.contains(string)) {
-                        Minecraft.getMinecraft().thePlayer.sendChatMessage("/stats " + string);
-                        playerNames.remove(string);
-                        StatsAddon.getInstance().checkedPlayers.add(string);
-                        try {
-                            Thread.sleep(StatsAddon.getInstance().cooldown);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
+                if (StatsAddon.getInstance().getCurrentGamemode() != null) {
+                    if (!string.equals(Minecraft.getMinecraft().thePlayer.getGameProfile().getName())) {
+                        if (!StatsAddon.getInstance().checkedPlayers.contains(string)) {
+                            Minecraft.getMinecraft().thePlayer.sendChatMessage("/stats " + string);
+                            playerNames.remove(string);
+                            StatsAddon.getInstance().checkedPlayers.add(string);
+                            try {
+                                Thread.sleep(StatsAddon.getInstance().cooldown);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            playerNames.remove(string);
                         }
-                    } else {
-                        playerNames.remove(string);
                     }
-                }
+                } else
+                    break;
             }
         }).start();
     }
