b84b328db3807419c403a80b3a4465b814024737
Lezurex <lenny.angst@easyid.ch>
2021-01-30 19:02:49 +0100

    Revert "Merge nick detector into master branch"
    This reverts commit 6271389f

--- src/main/java/com/voxcrafterlp/statsaddon/utils/StatsDisplayUtil.java
+++ src/main/java/com/voxcrafterlp/statsaddon/utils/StatsDisplayUtil.java
@@ -1,14 +1,8 @@
 package com.voxcrafterlp.statsaddon.utils;
 
 import com.voxcrafterlp.statsaddon.StatsAddon;
-import com.voxcrafterlp.statsaddon.nickedchecker.ClanChecker;
-import com.voxcrafterlp.statsaddon.nickedchecker.NickAlgorithm;
-import com.voxcrafterlp.statsaddon.nickedchecker.PremiumChecker;
-import net.labymod.main.LabyMod;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.network.NetworkPlayerInfo;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,71 +14,28 @@ import java.util.List;
 
 public class StatsDisplayUtil {
 
-    private ArrayList<NickAlgorithm> nickAlgorithms;
-
-    public StatsDisplayUtil() {
-
-    }
-
-    public void displayStats(List<NetworkPlayerInfo> playerNames, String joinMessage) {
+    public void displayStats(List<String> playerNames) {
         new Thread(() -> {
-            for(NetworkPlayerInfo playerInfo : playerNames) {
-                if (!playerInfo.getPlayerTeam().getColorSuffix().toLowerCase().replace("i", "y").replace("á", "a").contains("party")) {
-                    if (!playerInfo.getGameProfile().getName().equals(Minecraft.getMinecraft().thePlayer.getGameProfile().getName())) {
-                        if (!StatsAddon.getInstance().checkedPlayers.contains(playerInfo)) {
-                            Minecraft.getMinecraft().thePlayer.sendChatMessage("/" + this.getCommand(playerInfo.getGameProfile().getName()));
-                            playerNames.remove(playerInfo);
-                            StatsAddon.getInstance().checkedPlayers.add(playerInfo);
+            for(String string: playerNames) {
+                if (StatsAddon.getInstance().getCurrentGamemode() != null) {
+                    if (!string.equals(Minecraft.getMinecraft().thePlayer.getGameProfile().getName())) {
+                        if (!StatsAddon.getInstance().checkedPlayers.contains(string)) {
+                            Minecraft.getMinecraft().thePlayer.sendChatMessage("/" + this.getCommand(string));
+                            playerNames.remove(string);
+                            StatsAddon.getInstance().checkedPlayers.add(string);
                             try {
                                 Thread.sleep(StatsAddon.getInstance().cooldown);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
                         } else {
-                            playerNames.remove(playerInfo);
+                            playerNames.remove(string);
                         }
                     }
-                } else {
-                    playerNames.remove(playerInfo);
-                    StatsAddon.getInstance().checkedPlayers.add(playerInfo);
-                }
+                } else
+                    break;
             }
         }).start();
-    }
-
-    public void checkNicked(NetworkPlayerInfo playerInfo, String joinMessage) {
-        System.out.println("start checker");
-        nickAlgorithms = new ArrayList<>();
-        nickAlgorithms.add(new ClanChecker());
-        if (joinMessage != null) {
-            nickAlgorithms.add(new PremiumChecker());
-        }
-
-        int points = 0;
-        int max = nickAlgorithms.size();
-        for (NickAlgorithm algorithm : nickAlgorithms) {
-            System.out.println(algorithm.getClass().getName());
-            boolean test = algorithm.performCheck(playerInfo, joinMessage);
-            System.out.println("Test: " + test);
-            if (test) {
-                points++;
-            }
-        }
-
-        float percentage;
-
-        if (points != 0) {
-            percentage = (max / points) * 100;
-        } else {
-            percentage = 0;
-        }
-
-        if (percentage >= 50) {
-            LabyMod.getInstance().displayMessageInChat(StatsAddon.getInstance().getPrefix() + "\u00A74Achtung! \u00A77Potentiell genickter Spieler\u00A77!");
-            LabyMod.getInstance().displayMessageInChat(StatsAddon.getInstance().getPrefix() + "\u00A77Name\u00A78: \u00A7c" + playerInfo.getGameProfile().getName() +
-                    " \u00A77Wahrscheinlichkeit\u00A78: \u00A7c" + percentage + "%" + " \u00A77Punkte\u00A78: \u00A7c" + points);
-        }
-
     }
 
     private String getCommand(String playerName) {
