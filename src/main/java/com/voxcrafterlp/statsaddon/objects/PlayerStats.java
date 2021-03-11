0ef4a854ce25b64c3252e2e43ba0bb79605031a9
Lezurex <lenny.angst@easyid.ch>
2021-03-11 22:14:28 +0100

    Improve client data evaluation Fix #17

--- src/main/java/com/voxcrafterlp/statsaddon/objects/PlayerStats.java
+++ src/main/java/com/voxcrafterlp/statsaddon/objects/PlayerStats.java
@@ -149,6 +149,7 @@ public class PlayerStats {
         jsonObject.addProperty("warned", warned);
         jsonObject.addProperty("rank", rank);
         jsonObject.addProperty("winRate", winRate);
+        jsonObject.addProperty("statsHidden", statsHidden);
         return jsonObject;
     }
 
--- src/main/resources/webapp/app/objects/Player.js
+++ src/main/resources/webapp/app/objects/Player.js
@@ -4,15 +4,17 @@ export default class Player {
     warned;
     rank;
     winRate;
+    statsHidden;
 
-    constructor(playerName, warned, rank, winrate) {
+    constructor(playerName, warned, rank, winRate, statsHidden) {
         this.playerName = playerName;
         this.warned = warned;
         this.rank = rank;
-        this.winRate = winrate;
+        this.winRate = winRate;
+        this.statsHidden = statsHidden;
     }
 
     static fromObject(obj) {
-        return new Player(obj.name, obj.warned, obj.rank, obj.winRate)
+        return new Player(obj.name, obj.warned, obj.rank, obj.winRate, obj.statsHidden);
     }
 }
