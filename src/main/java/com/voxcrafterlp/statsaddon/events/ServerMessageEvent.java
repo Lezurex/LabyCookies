9dc3584ffdb5102e3e14967065a4e5cca5c3f30e
VoxCrafterLP <58361830+VoxCrafterLP@users.noreply.github.com>
2021-03-12 07:42:04 +0100

    Fix players not being checked when their name CONTAINS the addon's user's name

--- src/main/java/com/voxcrafterlp/statsaddon/events/ServerMessageEvent.java
+++ src/main/java/com/voxcrafterlp/statsaddon/events/ServerMessageEvent.java
@@ -35,7 +35,7 @@ public class ServerMessageEvent {
                                 }
 
                                 LabyModCore.getMinecraft().getPlayer().sendQueue.getPlayerInfoMap().forEach((loadedPlayer) -> {
-                                    if(!StatsAddon.getInstance().getLoadedPlayerStats().containsKey(loadedPlayer.getGameProfile().getName()) && !loadedPlayer.getGameProfile().getName().contains(LabyMod.getInstance().getPlayerName()))
+                                    if(!StatsAddon.getInstance().getLoadedPlayerStats().containsKey(loadedPlayer.getGameProfile().getName()) && !loadedPlayer.getGameProfile().getName().equals(LabyMod.getInstance().getPlayerName()))
                                         StatsAddon.getInstance().getLoadedPlayerStats().put(loadedPlayer.getGameProfile().getName(), new PlayerStats(loadedPlayer));
                                 });
                             }).start();
